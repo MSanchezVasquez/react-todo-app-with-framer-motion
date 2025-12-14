aa81a84edd3b961d7947e7e9a1546148c2f8f702
Moises Sanchez <140144292+MSanchezVasquez@users.noreply.github.com>
2025-12-13 19:07:18 -0500

    fix: resolve editing bug, optimize ID generation and fix reorder logic
    - Fix TodoItem prop mismatch: passed 'todoText' instead of 'text' to enable editing.
    - Fix TodoList: disable drag-and-drop reordering when filters are active to prevent data loss.
    - Refactor reducer: replace Math.random() with crypto.randomUUID() for safer unique IDs.
    - Chore: remove unused variables in Header component.

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
-import { TodoItem } from './TodoItem'
-import { getFilteredTodos } from '../stores/util'
-import { Reorder } from 'framer-motion'
+import { TodoItem } from "./TodoItem";
+import { getFilteredTodos } from "../stores/util";
+import { Reorder, AnimatePresence } from "framer-motion";
 
-export const TodoList = ({ todos, visibilityFilter, setTodos }) => (
-  <>
-    <Reorder.Group className='todo-list' axis='y' values={todos} onReorder={setTodos}>
-      {getFilteredTodos(todos, visibilityFilter).map((todo, index) => (
-        <Reorder.Item key={todo.id} value={todo}>
-          <TodoItem index={index} todo={todo} />
-        </Reorder.Item>
-      ))}
+export const TodoList = ({ todos, visibilityFilter, setTodos }) => {
+  const filteredTodos = getFilteredTodos(todos, visibilityFilter);
+  const isReorderEnabled = visibilityFilter === "All";
+
+  if (!isReorderEnabled) {
+    return (
+      <ul className="todo-list">
+        <AnimatePresence>
+          {filteredTodos.map((todo, index) => (
+            <TodoItem key={todo.id} index={index} todo={todo} />
+          ))}
+        </AnimatePresence>
+      </ul>
+    );
+  }
+
+  return (
+    <Reorder.Group
+      className="todo-list"
+      axis="y"
+      values={todos}
+      onReorder={setTodos}
+    >
+      <AnimatePresence>
+        {filteredTodos.map((todo, index) => (
+          <Reorder.Item key={todo.id} value={todo}>
+            <TodoItem index={index} todo={todo} />
+          </Reorder.Item>
+        ))}
+      </AnimatePresence>
     </Reorder.Group>
-  </>
-)
+  );
+};
